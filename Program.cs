d95dd021d02b64b4c810b00e8bd579b8717aa937
Илья Морозкин <morozkin.ilya86@mail.ru>
2022-02-07 17:45:55 +0300

    задача2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int firstNumber = new Random().Next(1, 100);
+Console.WriteLine("$Первое число {firstNumber}");
+int secondNumber = new Random().Next(1, 100);
+Console.WriteLine("$Второе число {secondNumber}");
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("$Большее число {firstNumber}");
+    Console.WriteLine("$Меньшее число {secondNumber}");
+}
+else
+{
+    Console.WriteLine("$Большее число {secondNumber}");
+    Console.WriteLine("$Меньшее число {firstNumber}");
+}
